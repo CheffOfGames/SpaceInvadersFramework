4b34fb9e0729beb1865208ce98374bedd7cc8c4d
CheffOfGames <jeffreyhollander1999@gmail.com>
2017-03-07 10:51:34 +0100

    Step 2.3 - Start
    Laat de ‘Player’ ​onder in het scherm naar links en naar rechts bewegen door middel van de pijltjestoetsen (1/2 pt)

--- SpaceInvadersFramework/SpaceInvadersFramework/Player.cs
+++ SpaceInvadersFramework/SpaceInvadersFramework/Player.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework.Input;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,19 @@ namespace SpaceInvadersFramework
         public Player(string assetname, int layer = 0, string id = "", int sheetIndex = 0) : base(assetname, layer, id, sheetIndex)
         {
             assetname = "ship";
+        }
+
+        public override void HandleInput(InputHelper inputHelper)
+        {
+            if (inputHelper.KeyPressed(Keys.Left))
+            {
+                this.position.X--;
+            }
+
+            if (inputHelper.KeyPressed(Keys.Right))
+            {
+                this.position.X++;
+            }
         }
     }
 }
